78295efa4cb2583089498ba97585eb0a63d8ded5
jsirianni <jsirianni@ws1.sir.local>
2017-09-24 19:53:46 -0400

    fix upgrade script

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -6,6 +6,6 @@ sudo git status
 read -p "Press [Enter] key to upgrade repopull on this branch"
 sudo service repopull stop
 sudo rm /usr/local/bin/repopull
-sudo cp ./bin/repopull /usr/local/bin/
+sudo cp repopull /usr/local/bin/
 sudo service repopull start
 sudo service repopull status
